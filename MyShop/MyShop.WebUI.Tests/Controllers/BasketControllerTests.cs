c496bf395d12465679e165d4f434f8259360c8f5
silotech-it <silotech@silotech-it.com.br>
2019-01-22 16:41:51 -0300

    Unit test didnt work but... Got the idea

---
+++ MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.WebUI.Tests.Mocks;
+using MyShop.Services;
+using System.Linq;
+using MyShop.WebUI.Controllers;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    /// <summary>
+    /// Descrição resumida para BasketControllerTests
+    /// </summary>
+    [TestClass]
+    public class BasketControllerTests
+    {
+
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+            var controller = new BasketController(basketService);
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            //basketService.AddToBasket(httpContext, "1");
+            controller.AddToBasket("1");
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            Assert.IsNull(basket);
+        }
+    }
+}
