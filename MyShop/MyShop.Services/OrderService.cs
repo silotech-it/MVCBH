8c55f0328bc8b220cf0bbe0dd26246099d301053
silotech-it <silotech@silotech-it.com.br>
2019-01-23 18:08:25 -0300

    DisplayOrders P1 and P2 Finished

--- MyShop/MyShop.Services/OrderService.cs
+++ MyShop/MyShop.Services/OrderService.cs
@@ -33,7 +33,22 @@ namespace MyShop.Services
             }
             orderContext.Insert(baseOrder);
             orderContext.Commit();
+        }
 
+        public List<Order> GetOrderList()
+        {
+            return orderContext.Collection().ToList();
+        }
+
+        public Order GetOtrder(string Id)
+        {
+            return orderContext.Find(Id);
+        }
+
+        public void UpdateOrder(Order updateOrder)
+        {
+            orderContext.Update(updateOrder);
+            orderContext.Commit();
         }
     }
 }
